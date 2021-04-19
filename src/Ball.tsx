ecc80b039eda319678b97e101b1a59c245fdeb4e
bekazandukeli <bekazandukeli@gmail.com>
2021-04-19 09:28:32 +0400

    first animation

---
+++ src/Ball.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
+
+export default function Ball() {
+  const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  useEffect(() => {
+    Animated.spring(position, {
+      toValue: { x: 200, y: 500 },
+      useNativeDriver: false,
+    }).start();
+  }, []);
+  return (
+    <Animated.View style={position.getLayout()}>
+      <View style={styles.ball} />
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  ball: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: 'black',
+  },
+});
