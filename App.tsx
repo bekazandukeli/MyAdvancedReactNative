e82c375d99c0d09500bf74e04852ca33912b2a5b
bekazandukeli <bekazandukeli@gmail.com>
2021-07-05 10:29:22 +0300

    videos 16-33

--- App.tsx
+++ App.tsx
@@ -1,62 +1,87 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Deck from './src/Deck';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Card, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const DATA = [
   {
     id: 1,
     text: 'Card #1',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 2,
     text: 'Card #2',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 3,
     text: 'Card #3',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 4,
     text: 'Card #4',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 5,
     text: 'Card #5',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 6,
     text: 'Card #6',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 7,
     text: 'Card #7',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
   {
     id: 8,
     text: 'Card #8',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg',
+    uri: 'https://picsum.photos/200/300',
   },
 ];
 
-export interface RenderCardProps {
+export interface CardData {
+  id: number;
   text: string;
+  uri: string;
 }
-
 export default function App() {
-  function renderCard(item: RenderCardProps) {
-    return <Text>{item.text}</Text>;
+  function renderCard(item: CardData) {
+    return (
+      <Card key={item.id}>
+        <Card.Title>{item.text}</Card.Title>
+        <Card.Image source={{ uri: item.uri }} />
+        <Text style={{ marginBottom: 10 }}>I can customize card further</Text>
+        <Button
+          title="View Now"
+          icon={
+            <Icon
+              name="code"
+              size={15}
+              color="white"
+              style={{ marginRight: 10 }}
+            />
+          }
+          buttonStyle={{ backgroundColor: '#03A9F4' }}
+        />
+      </Card>
+    );
   }
+
   return (
-    <View style={styles.container}>
-      <Deck data={DATA} renderCard={renderCard} />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Deck data={DATA} renderCard={renderCard} />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,19 +1,103 @@
-import React, { ReactNode } from 'react';
-import { View, Animated } from 'react-native';
-import { RenderCardProps } from '../App';
+import React, { useRef, useState } from 'react';
+import {
+  View,
+  PanResponder,
+  Animated,
+  StyleProp,
+  ViewStyle,
+  Dimensions,
+  RegisteredStyle,
+} from 'react-native';
+import { CardData } from '../App';
 
-interface CardData {
-  id: number;
-  text: string;
-  uri: string;
-}
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = SCREEN_WIDTH * 0.25;
+
 interface Props {
   data: CardData[];
-  renderCard: (item: RenderCardProps) => JSX.Element;
+  renderCard: (item: CardData) => JSX.Element;
+  onSwipeLeft: () => void;
+  onSwipeRight: () => void;
 }
-export default function Deck({ renderCard, data }: Props) {
-  function renderCards() {
-    return data.map((item) => renderCard(item));
+export default function Deck({
+  renderCard,
+  data,
+  onSwipeLeft = () => null,
+  onSwipeRight = () => null,
+}: Props) {
+  const position = useRef(new Animated.ValueXY()).current;
+  const [itemIndex, setItemIndex] = useState(0);
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (evt, gestureState) => {
+        position.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        if (gestureState.dx > SWIPE_THRESHOLD) {
+          forceSwipe('right');
+        } else if (gestureState.dx < -SWIPE_THRESHOLD) {
+          forceSwipe('left');
+        } else {
+          resetPosition();
+        }
+      },
+    })
+  ).current;
+
+  type Direction = 'right' | 'left';
+  function forceSwipe(direction: Direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH - 25;
+    Animated.timing(position, {
+      toValue: { x, y: 0 },
+      duration: 200,
+      useNativeDriver: false,
+    }).start(() => onSwipeComplete(direction));
   }
+
+  function onSwipeComplete(direction: Direction) {
+    const item = data[itemIndex];
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    position.setValue({ x: 0, y: 0 });
+    setItemIndex((prev) => prev + 1);
+  }
+
+  function resetPosition() {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+      useNativeDriver: false,
+    }).start();
+  }
+
+  function getCardStyle():
+    | Animated.Value
+    | Animated.AnimatedInterpolation
+    | RegisteredStyle<ViewStyle>
+    | Animated.WithAnimatedObject<ViewStyle>
+    | Animated.WithAnimatedArray<ViewStyle> {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-60deg', '0deg', '60deg'],
+    });
+    return [position.getLayout(), { transform: [{ rotate }] }];
+  }
+
+  function renderCards() {
+    return data.map((item, index) => {
+      if (index === itemIndex) {
+        return (
+          <Animated.View
+            style={[getCardStyle()]}
+            key={item.id}
+            {...panResponder.panHandlers}
+          >
+            {renderCard(item)}
+          </Animated.View>
+        );
+      }
+      return renderCard(item);
+    });
+  }
+
   return <View>{renderCards()}</View>;
 }
